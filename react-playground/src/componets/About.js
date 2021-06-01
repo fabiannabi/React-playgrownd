0997adb8fb4056ae4635529c8fa97cec5ccd98bd
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-01 14:24:48 -0500

    Placeholder componets for routing

---
+++ react-playground/src/componets/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className= "container">
+            <h4 className="center text-blue">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus felis non lacus ultrices, quis auctor sapien aliquam. Ut sit amet libero quis elit vulputate cursus quis et velit. Donec elementum nec tellus scelerisque condimentum. Etiam bibendum massa est, ac tristique diam pulvinar vitae. Donec id nibh ut odio posuere malesuada vel vel ex. Mauris sodales aliquet metus, eget pharetra purus venenatis vitae. Integer at lectus dolor. Praesent dapibus porttitor enim vitae egestas. Suspendisse potenti.</p>
+
+        </div>
+    )
+}
+
+export default About
