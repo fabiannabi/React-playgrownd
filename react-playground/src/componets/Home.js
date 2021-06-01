58813124827d737a98493b7ab8a6d1977057a476
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-01 17:47:33 -0500

    Adds the content from an api with axios

--- react-playground/src/componets/Home.js
+++ react-playground/src/componets/Home.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return(
-        <div className= "container">
-            <h4 className="center text-blue">HOME</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tempus felis non lacus ultrices, quis auctor sapien aliquam. Ut sit amet libero quis elit vulputate cursus quis et velit. Donec elementum nec tellus scelerisque condimentum. Etiam bibendum massa est, ac tristique diam pulvinar vitae. Donec id nibh ut odio posuere malesuada vel vel ex. Mauris sodales aliquet metus, eget pharetra purus venenatis vitae. Integer at lectus dolor. Praesent dapibus porttitor enim vitae egestas. Suspendisse potenti.</p>
 
-        </div>
-    )
+class Home extends Component {
+
+	state ={
+		post:[ ]
+	}
+	componentDidMount(){
+		axios.get('https://jsonplaceholder.typicode.com/posts/')
+			.then(response =>{
+				console.log(response)
+				this.setState({post: response.data.slice(0,10)})
+			})
+
+	}
+
+   render() {
+		 const {post} = this.state
+
+		 const postlist = post.length ? (post.map(post =>{
+			 return (
+				 <div className= "post card " key={post.id}>
+					 <div className= "card-content">
+						<span className ="card-title"> {post.title}</span>
+						<p> {post.body}</p>
+					</div>
+				 </div>
+			 )
+		 })) : (<h1 className= "center blue-text">There are no Post</h1>)
+			return (
+			<div className= "container">
+				<h4 className="center text-blue">HOME</h4>
+				{postlist}
+			</div>
+			)
+   }
 }
 
 export default Home
