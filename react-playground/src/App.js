a446b8c6e4182801f9aa9d38188c06a24631ad76
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-01 14:24:19 -0500

    Initial Set up

---
+++ react-playground/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+  state = {
+
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>hello</div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
