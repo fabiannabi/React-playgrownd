64463c3d625af97ea1f96b550cdaf7803eb027a6
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-02 12:43:02 -0500

    Update home to fetch from Redux instead of axios

--- react-playground/src/componets/Home.js
+++ react-playground/src/componets/Home.js
@@ -1,20 +1,10 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import Pokeball from '../pokeball.png';
 
 
 class Home extends Component {
-
-	state ={
-		post:[ ]
-	}
-	componentDidMount(){
-		axios.get('https://jsonplaceholder.typicode.com/posts/')
-			.then(response =>{
-				console.log(response)
-				this.setState({post: response.data.slice(0,10)})
-			})
-	}
 
    render() {
 		 const {post} = this.state
@@ -22,15 +12,16 @@ class Home extends Component {
 		 const postlist = post.length ? (post.map(post =>{
 			 return (
 				 <div className= "post card " key={post.id}>
+					 <img src={Pokeball} alt="pokeball"></img>
 					 <div className= "card-content">
-						<Link to={"/"+post.id}><span className ="card-title"> {post.title}</span></Link>
+						<Link to={"/"+post.id}><span className ="card-title red-text"> {post.title}</span></Link>
 						<p> {post.body}</p>
 					</div>
 				 </div>
 			 )
 		 })) : (<h1 className= "center blue-text">There are no Post</h1>)
 			return (
-			<div className= "container">
+			<div className= "container home">
 				<h4 className="center text-blue">HOME</h4>
 				{postlist}
 			</div>
