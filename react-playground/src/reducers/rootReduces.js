149e10675d90101c8627a8166195254b06343069
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-02 12:43:12 -0500

    Create root Reducer

---
+++ react-playground/src/reducers/rootReduces.js
@@ -0,0 +1,13 @@
+const initialState = {
+	post: [
+		{id:1, title: "title 1", body: "body 1"},
+		{id:2, title: "title 2", body: "body 2"},
+		{id:3, title: "title 3", body: "body 3"}
+	]
+}
+
+const rootReducer = (state = initialState, action) => {
+	return state;
+}
+
+export default rootReducer
