0f0fc49e9a9409ddb36432ed5ce2fea1932ececa
Fabian Alcala <fabiannabi@yahoo.com.mx>
2021-06-02 14:16:32 -0500

    Update to use Acition Creator to delete post

--- react-playground/src/componets/Post.js
+++ react-playground/src/componets/Post.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import {connect} from 'react-redux'
+import {deletePost} from '../actions/postActions'
 
 class Post extends Component {
 
@@ -50,7 +51,7 @@ const mapStateToProps =(state, ownProps) =>{
 const mapDispatchToProps = (dispatch) => {
 	return {
 		deletePost: (id) =>{
-			dispatch({type:"DELETE_POST", id:id})
+			dispatch(deletePost(id));
 		}
 	}
 }
